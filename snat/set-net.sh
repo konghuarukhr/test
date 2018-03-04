b4ab811c7091c63338ee0c9cb4087cccb18e2472
Dustin Zheng <konghuarukhr@163.com>
2018-03-04 20:22:52 +0800

    add src files

--- snat/set-net.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# https://www.kernel.org/doc/Documentation/networking/ip-sysctl.txt
-sysctl net.ipv4.conf.all.rp_filter=0
-sysctl net.ipv4.conf.all.accept_local=1
-sysctl net.ipv4.ip_forward=1
