e325ba094bfc310a1c3e9261851fb405e3bf7fef
Dustin Zheng <konghuarukhr@163.com>
2018-05-06 16:53:08 +0800

    optimize 1

--- scripts/set-client-dns.sh
+++ scripts/set-client-dns.sh
@@ -2,3 +2,4 @@
 
 iptables -tnat -AOUTPUT -pudp --dport 53 -jDNAT --to-destination 8.8.4.4
 iptables -tnat -APREROUTING -pudp --dport 53 -jDNAT --to-destination 8.8.4.4
+iptables -tnat -APOSTROUTING -o enp0s3 -j MASQUERADE
